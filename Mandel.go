ed777833b84246633065d1b8787a7ffb5d0d1de8
jfhaecker <jfhaeckergmail.com>
2019-11-03 17:20:23 +0100

    Colors fading more smoothly

--- Mandel.go
+++ Mandel.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"math/cmplx"
 	"os"
 	"runtime"
@@ -16,10 +17,10 @@ var (
 	imageWidth                   = 1000
 	imageHeight                  = 1000
 	maxIterStart                 = 1000
-	bailoutRadius        float64 = 2000
+	bailoutRadius        float64 = 20
 	maxMandelWorkerCount         = runtime.GOMAXPROCS(0)
 	maxImageWorkerCount          = 1 // see SyncImage
-	imageCount           int     = 5
+	imageCount           int     = 1000
 )
 
 type ComplexRectangle struct {
@@ -51,9 +52,11 @@ func (r *ComplexRectangle) calc() {
 }
 
 type ComplexPoint struct {
-	z          complex128
-	iterations int
-	x, y       int
+	z                  complex128
+	iterationCount     int
+	normIterationCount float64
+	frac               float64
+	x, y               int
 }
 
 type SyncImage struct {
@@ -80,12 +83,18 @@ func mandelbrot(point *ComplexPoint, maxIter int) *ComplexPoint {
 	c, zz := point.z, point.z
 	for iter := 1; ; iter++ {
 		zz = zz*zz + c
-		point.iterations = iter
+		point.iterationCount = iter
+		absz := cmplx.Abs(zz)
 
-		// TODO: float mu = iter_count - (log (log (modulus)))/ log (2.0);
-		// https://linas.org/art-gallery/escape/escape.html
+		if absz > bailoutRadius {
+			// https://linas.org/art-gallery/escape/escape.html
+			log_zn := math.Log10(absz)
+			nu := math.Log10(log_zn/math.Log10(2)) / math.Log10(2)
+			// https://en.wikipedia.org/wiki/Mandelbrot_set#Continuous_.28smooth.29_coloring
 
-		if cmplx.Abs(zz) > bailoutRadius {
+			point.normIterationCount = float64(float64(iter) + 1.0 - nu)
+			_, frac := math.Modf(point.normIterationCount)
+			point.frac = frac
 			return point
 		}
 		if iter == maxIter {
@@ -99,14 +108,44 @@ func getColor(index int) color.RGBA {
 	return qu[(index)%len(qu)]
 }
 
+func colorInterpolate(c1 color.RGBA, c2 color.RGBA, frac float64) color.RGBA {
+	c1_r := c1.R
+	c2_r := c2.R
+	c1_g := c1.G
+	c2_g := c2.G
+	c1_b := c1.B
+	c2_b := c2.B
+
+	c_r := c1_r + uint8(float64(c2_r-c1_r)*frac)
+	c_g := c1_g + uint8(float64(c2_g-c1_g)*frac)
+	c_b := c1_b + uint8(float64(c2_b-c1_b)*frac)
+	c := color.RGBA{c_r, c_g, c_b, 255}
+	return c
+}
+
 func renderImage(points <-chan *ComplexPoint, wg *sync.WaitGroup, fileName string, syncImage *SyncImage, maxIter int) {
 	defer wg.Done()
 	for point := range points {
 		co := color.RGBA{0, 0, 0, 0}
-		if point.iterations == maxIter {
-			co = color.RGBA{255, 255, 255, 255}
+		if point.iterationCount == maxIter {
+			co = color.RGBA{0, 0, 0, 255}
 		} else {
-			co = getColor(point.iterations)
+
+			// https://en.wikipedia.org/wiki/Mandelbrot_set#Continuous_.28smooth.29_coloring
+			c1 := getColor(int(math.Floor(point.normIterationCount)))
+			c2 := getColor(int(math.Floor(point.normIterationCount) + 1))
+
+			co = colorInterpolate(c1, c2, point.frac)
+			/*if point.x > 800 && point.x < 950 && point.y == 500 {
+				fmt.Println("hallo")
+				fmt.Printf("Color1%v Color2%v => %v\n", c1, c2, co)
+
+				fmt.Printf("%v %v %v %v %v \n", point.x, point.y,
+					point.iterationCount,
+					point.normIterationCount,
+					point.frac)
+
+			}*/
 		}
 		syncImage.SetUnlocked(point.x, point.y, co)
 	}
@@ -137,9 +176,10 @@ func main() {
 	fmt.Printf("Using %v mandelworkers and %v imageworkers for %v images\n", maxImageWorkerCount, maxMandelWorkerCount, imageCount)
 
 	maxIter := maxIterStart
-	start := locations[0]
+	start := locations[18]
 	rectangle := &ComplexRectangle{}
-	rectangle.Set(complex(start.X, start.Y), start.R, start.R)
+	rectangle.Set(complex(start.X, start.Y), 0.1, 0.1)
+	//rectangle.Set(complex(0.0, 0.0), 4.0, 4.0)
 
 	syncImage := &SyncImage{image: image.NewRGBA(image.Rectangle{
 		image.Point{0, 0},
